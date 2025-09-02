0b774cdd2bb8416e7f97bdb4a76c316cf902c8c3
ftsuda-senac <ftsuda.senac@gmail.com>
2025-09-01 21:34:01 -0300

    Criação do Bean com @Configuration + @Bean

--- exemplos-spring-inicial/src/main/java/br/senac/tads/dsw/exemplos/ExemplosSpringInicialApplication.java
+++ exemplos-spring-inicial/src/main/java/br/senac/tads/dsw/exemplos/ExemplosSpringInicialApplication.java
@@ -2,12 +2,19 @@ package br.senac.tads.dsw.exemplos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication // @Configuration implicito
 public class ExemplosSpringInicialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExemplosSpringInicialApplication.class, args);
+	}
+
+	// Objeto retornado passa a ser gerenciado pelo Spring
+	@Bean
+	public GeradorSaida geradorSaida() {
+		return new GeradorSaidaJson();
 	}
 
 }
