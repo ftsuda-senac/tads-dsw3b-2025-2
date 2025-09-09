0a045e4f70a31af39633aeeb910f97197c74c28e
ftsuda-senac <ftsuda.senac@gmail.com>
2025-09-08 22:39:44 -0300

    Criação do Dto e Service

---
+++ dados-pessoais/src/main/java/br/senac/tads/dsw/dadospessoais/PessoaDto.java
@@ -0,0 +1,92 @@
+package br.senac.tads.dsw.dadospessoais;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class PessoaDto {
+
+    private String username;
+
+    private String nome;
+
+    private LocalDate dataNascimento;
+
+    private String email;
+
+    private String telefone;
+
+    private List<String> interesses;
+
+    private String senha;
+
+    public PessoaDto() {
+    }
+
+    public PessoaDto(String username, String nome, LocalDate dataNascimento, String email, String telefone,
+            List<String> interesses, String senha) {
+        this.username = username;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+        this.telefone = telefone;
+        this.interesses = interesses;
+        this.senha = senha;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public List<String> getInteresses() {
+        return interesses;
+    }
+
+    public void setInteresses(List<String> interesses) {
+        this.interesses = interesses;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
---
+++ dados-pessoais/src/main/java/br/senac/tads/dsw/dadospessoais/PessoaService.java
@@ -0,0 +1,53 @@
+package br.senac.tads.dsw.dadospessoais;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+public class PessoaService {
+
+    private Map<String, PessoaDto> mapPessoas;
+
+    public PessoaService() {
+        mapPessoas = new HashMap<>();
+        mapPessoas.put("fulano",
+            new PessoaDto("fulano", "Fulano da Silva", LocalDate.parse("2000-10-20"),
+            "fulano@email.com", "11 91234-1234", List.of("Java", "Spring Boot"),
+            "Abcd1234"));
+        mapPessoas.put("ciclano",
+            new PessoaDto("ciclano", "Ciclano de Souza", LocalDate.parse("1999-05-25"),
+            "ciclano@email.com", "11 98765-8765", List.of("HTML", "CSS", "Javascript"),
+            "Abcd1234"));
+        mapPessoas.put("beltrana",
+            new PessoaDto("beltrana", "Beltrana dos Santos", LocalDate.parse("2001-02-14"),
+            "beltrana@email.com", "11 91122-1122", List.of("Javascript", "React", "Angular"),
+            "Abcd1234"));
+    }
+
+    public List<PessoaDto> findAll() {
+        return new ArrayList<>(mapPessoas.values());
+    }
+
+    public PessoaDto findByUsername(String username) {
+        return mapPessoas.get(username);
+    }
+
+    public PessoaDto addNew(PessoaDto dto) {
+        return null;
+    }
+
+    public PessoaDto update(String username, PessoaDto dto) {
+        return null;
+    }
+
+    public void delete(String username) {
+
+    }
+}
